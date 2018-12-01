97c7beb642c65eb725c5c85a497223074a351e95
David Roszkowski <trumpetcrazy@gmail.com>
2018-11-30 21:27:31 -0600

    Update, Not sure why. I'm not sure what's changed, but I'm switching computers.

--- src/main/java/hello/SudokuBoard.java
+++ src/main/java/hello/SudokuBoard.java
@@ -3,14 +3,12 @@ package hello;
 import java.util.ArrayList;
 import java.util.List;
 
-enum {
-
-        }
 class SudokuBoard {
-    private int[][] board = new int[9][9];
+    static private SudokuCell[][] board = new SudokuCell[9][9];
     private LexicalEnvironment[] squares, rows, colums;
 
     SudokuBoard(String[] inputArray) {
+        setBoard(inputArray);
         String Breakpoint = "break";
     }
 
@@ -19,16 +17,67 @@ class SudokuBoard {
         for(int row=0; row<9; row++){
             for (int col=0; col<9; col++) {
                 if (inputArray[iterator] != "") {
-                    board[row][col] = Integer.parseInt(inputArray[iterator]);
+                    board[row][col] = new SudokuCell(Integer.parseInt(inputArray[iterator]), row, col);
+                }
+                else {
+                    board[row][col] = new SudokuCell(0, row, col);
                 }
                 iterator++;
             }
         }
     }
 
-    private void setEnvironments() {
+    private void buildEnvironments() {
+        for (int x=9; x<9; x++) {
+            LexicalEnvironment le = new LexicalEnvironment();
+        }
+    }
+}
+
+class SudokuCell {
+    private int x, y, value;
+    private Candidates candidates;
+
+    {candidates = new Candidates();}
+
+    public SudokuCell() {
+        super();
+    }
+
+    public SudokuCell(int value, int x, int y) {
+        this();
+        this.value = value;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setValue(int value) {
+       this.value = value;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+}
+
+class Candidates {
+    private int [] candidates;
+
+    Candidates() {
+        candidates = new int[9];
+    }
+
+    public void update() {
 
     }
+
+    public int[] getCandidates(){
+        return candidates;
+    }
+}
+
+class EnvironmentFactory {
+
 }
 
 interface LexicalEnvironmentStrategy {
